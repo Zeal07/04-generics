a727569dc9e279ed833279efb636de542f801ec8
Alex <zeeb.alexander@yahoo.com>
2020-01-13 19:53:49 +0100

    solution w/o optionally

---
+++ src/main/java/de/thro/inf/prg3/a04/collections/CollectionsUtility.java
@@ -0,0 +1,21 @@
+package de.thro.inf.prg3.a04.collections;
+
+import java.util.Comparator;
+import java.util.Iterator;
+
+public abstract class CollectionsUtility<T> {
+
+    public static <T> SimpleList<T> sort(SimpleList<T> list, Comparator<T> cmp){
+
+        SimpleList<T> result;
+
+        Iterator<T> iterator = list.iterator();
+
+        while(iterator.hasNext()){
+
+        }
+
+
+        return null;
+    }
+}
--- src/main/java/de/thro/inf/prg3/a04/collections/SimpleList.java
+++ src/main/java/de/thro/inf/prg3/a04/collections/SimpleList.java
@@ -1,19 +1,56 @@
 package de.thro.inf.prg3.a04.collections;
 
-public interface SimpleList extends Iterable {
+import java.util.function.Function;
+
+public interface SimpleList<T> extends Iterable<T> {
 	/**
 	 * Add a given object to the back of the list.
 	 */
-	void add(Object o);
+	void add(T o);
 
 	/**
 	 * @return current size of the list
 	 */
 	int size();
 
+    /**
+     * @return current Item T of the List
+     */
+	T getItem();
+
 	/**
-	 * Generate a new list using the given filter instance.
-	 * @return a new, filtered list
+     * @return next Element
 	 */
-	SimpleList filter(SimpleFilter filter);
+	T getNext();
+
+    /**
+     * Get a new SimpleList instance with all items of this list which match the given filter
+     * @param filter SimpleFilter instance
+     * @return new SimpleList instance
+     */
+	default SimpleList<T> filter(SimpleFilter<T> filter){
+        SimpleList<T> result = new SimpleListImpl<T>();
+        for(T o : this){
+            if(filter.include(o)){
+                result.add(o);
+            }
+        }
+        return result;
+    }
+
+	default SimpleList<T> addDefault(){
+	    return new SimpleListImpl<>();
+    }
+
+	default <R> SimpleList<R> map(Function<T, R> transform){
+
+	    SimpleList<R> result = new SimpleListImpl<>();
+
+        for (T o: this) {
+            R tmp = transform.apply(o);
+            result.add(tmp);
+        }
+
+        return result;
+    }
 }
